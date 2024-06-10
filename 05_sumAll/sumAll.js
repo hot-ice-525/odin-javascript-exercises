4198fdeb6faa201f7bcdee273b458997e37e8934
lalit-machra <lalitmachra0403@gmail.com>
2024-06-11 00:31:32 +0530

    Solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+
+  let greater = num2 > num1;
+  let i = greater ? num1 : num2;
+
+  for (; i <= (greater ? num2 : num1); i++) {
+    sum += i;
+  }
+  
+  return sum;
 };
 
 // Do not edit below this line
