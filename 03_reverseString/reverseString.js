a094e000d0ea0f6ce89dd51676d56327404f8fab
lalit-machra <lalitmachra0403@gmail.com>
2024-06-10 16:55:09 +0530

    Solution to 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let strArray = Array.from(str);
+  let newStrArray = [];
 
+  for (let i = (strArray.length - 1); i >= 0; i--) {
+    newStrArray.push(strArray[i]);
+  }
+
+  let reverseStr = newStrArray.join("");
+  return reverseStr;
 };
 
 // Do not edit below this line
