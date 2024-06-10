c6a68afdc40736afc08171dad93ce0b2400f4990
lalit-machra <lalitmachra0403@gmail.com>
2024-06-10 21:55:22 +0530

    Solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+  for (let i = 0; i < elements.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (elements[i] === array[j]) {
+        array.splice(j, 1);
+        j -= 1;
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
