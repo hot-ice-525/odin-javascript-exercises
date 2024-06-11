a4ac9c86f2f190bc2254c223bf2a23aa335a0381
lalit-machra <lalitmachra0403@gmail.com>
2024-06-11 07:17:59 +0530

    Solution to 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return +((fahrenheit - 32) * (5 / 9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return +(((9 * celsius) / 5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
