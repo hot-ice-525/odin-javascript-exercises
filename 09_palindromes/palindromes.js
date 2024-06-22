e9db58d2fe7183833ecff941f0492153cf050369
lalit-machra <lalitmachra0403@gmail.com>
2024-06-22 17:45:43 +0530

    Solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let strArray = string.toLowerCase().split("");
 
+  let removeFromArray = [" ", ",", "'", "!", "."];
+  let str = strArray.filter((char) => !removeFromArray.includes(char)).join();
+
+  let reversedStr = str.split("").reverse().join("");
+
+  return (str === reversedStr);
 };
 
 // Do not edit below this line
