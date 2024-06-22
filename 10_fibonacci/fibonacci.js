587566740c3d01c5b5985725cd77799101f4be0e
lalit-machra <lalitmachra0403@gmail.com>
2024-06-22 19:31:38 +0530

    Solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  const sequenceUpto = +number;
+  if (sequenceUpto === 0) {
+    return 0;
+  }
+  else if (sequenceUpto < 0) {
+    return "OOPS";
+  }
+  else {
+    let currentNumber;
+    let fibonacciArr = [1];   
+    for (let i = 1; i < sequenceUpto; i++) {
+      if (i === 1) {
+        currentNumber = 0 + fibonacciArr[i - 1];
+      }
+      else {
+        currentNumber = fibonacciArr[i - 2] + fibonacciArr[i - 1];
+      }   
+      fibonacciArr.push(currentNumber);
+    }   
+    return fibonacciArr[sequenceUpto - 1];
+    }
 };
 
 // Do not edit below this line
