2a6e24ac27b3edc6dfcf40a025cd543aea1d2279
lalit-machra <lalitmachra0403@gmail.com>
2024-06-23 06:25:33 +0530

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldestAge = people.map((person, index) => {
+    if (!person["yearOfDeath"]) {
+      const date = new Date();
+      person["yearOfDeath"] = date.getFullYear();
+    }
 
+    let age = (person["yearOfDeath"] - person["yearOfBirth"]);
+    
+    return age;
+  }).sort((a, b) => b - a)[0];
+
+  let oldestPerson;
+
+  people.forEach((person) => {
+    if (person["yearOfDeath"] - person["yearOfBirth"] === oldestAge) {
+      oldestPerson = person;
+    }
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
